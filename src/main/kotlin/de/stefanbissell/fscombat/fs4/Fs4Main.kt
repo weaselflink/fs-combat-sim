4406f87d7dc2c29a9015d703c786de7bf29aef43
weaselflink <stefan@stefanbissell.de>
2021-08-22 17:16:09 +0200

    more interesting example

--- src/main/kotlin/de/stefanbissell/fscombat/fs4/Fs4Main.kt
+++ src/main/kotlin/de/stefanbissell/fscombat/fs4/Fs4Main.kt
@@ -3,19 +3,19 @@ package de.stefanbissell.fscombat.fs4
 fun main() {
     val playerA = Fs4Player(
         name = "A",
-        strength = 8,
-        melee = 8,
+        strength = 5,
+        melee = 5,
         armor = Fs4Armor.ThickClothing,
-        weapon = Fs4Weapon.Knife,
+        weapon = Fs4Weapon.Dagger,
         shield = Fs4Shield.Standard
     )
     val playerB = Fs4Player(
         name = "B",
-        strength = 8,
-        melee = 8,
-        armor = Fs4Armor.ThickClothing,
-        weapon = Fs4Weapon.GreatWeapon,
-        shield = Fs4Shield.Standard
+        strength = 5,
+        melee = 5,
+        armor = Fs4Armor.ScaleMail,
+        weapon = Fs4Weapon.Dagger,
+        shield = Fs4Shield.None
     )
 
     Fs4Simulator(playerA, playerB)
