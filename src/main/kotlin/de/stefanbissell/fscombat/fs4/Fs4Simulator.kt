76a65caf3a9fb5b517abd3ae5b2f6ca275ab5021
weaselflink <stefan@stefanbissell.de>
2021-08-23 08:51:08 +0200

    clean code

--- src/main/kotlin/de/stefanbissell/fscombat/fs4/Fs4Simulator.kt
+++ src/main/kotlin/de/stefanbissell/fscombat/fs4/Fs4Simulator.kt
@@ -1,6 +1,8 @@
 package de.stefanbissell.fscombat.fs4
 
 import de.stefanbissell.fscombat.percent
+import kotlin.math.max
+import kotlin.math.roundToInt
 
 class Fs4Simulator(
     private val playerAExpr: () -> Fs4Player,
@@ -8,18 +10,24 @@ class Fs4Simulator(
 ) {
 
     fun run(runs: Int): SimulationResult {
-        var vicA = 0
-        var vicB = 0
-        val rounds = mutableListOf<Int>()
+        val vicA = IntStatistic()
+        val vicB = IntStatistic()
+        val rounds = IntStatistic()
         repeat(runs) { index ->
             Fs4Combat(playerAExpr(), playerBExpr())
                 .run(index)
                 .also {
                     if (it.playerA.isAlive && !it.playerB.isAlive) {
-                        vicA++
+                        vicA += 1
+                        vicB += 0
                     }
                     if (!it.playerA.isAlive && it.playerB.isAlive) {
-                        vicB++
+                        vicA += 0
+                        vicB += 1
+                    }
+                    if (!it.playerA.isAlive && !it.playerB.isAlive) {
+                        vicA += 0
+                        vicB += 0
                     }
                     rounds += it.rounds
                 }
@@ -31,17 +39,34 @@ class Fs4Simulator(
 
 data class SimulationResult(
     val runs: Int,
-    val victoriesA: Int,
-    val victoriesB: Int,
-    val rounds: List<Int>,
-    val draws: Int = runs - (victoriesA + victoriesB)
+    val victoriesA: IntStatistic,
+    val victoriesB: IntStatistic,
+    val rounds: IntStatistic,
+    val draws: Int = runs - (victoriesA.sum + victoriesB.sum)
 ) {
 
     override fun toString(): String {
         return """
-            $victoriesA vs $victoriesB (draws $draws)
-            ${victoriesA percent runs}% vs ${victoriesB percent runs}% (draws ${draws percent runs}%)
-            Average rounds: ${rounds.average()}
+            ${victoriesA.sum} vs ${victoriesB.sum} (draws $draws)
+            ${victoriesA.percent}% vs ${victoriesB.percent}% (draws ${draws percent runs}%)
+            Average rounds: ${rounds.average}
         """.trimIndent()
     }
+}
+
+class IntStatistic {
+
+    private var count: Int = 0
+    var sum: Int = 0
+
+    operator fun plusAssign(value: Int) {
+        count++
+        sum += value
+    }
+
+    val average
+        get() = sum.toDouble() / max(1, count)
+
+    val percent
+        get() = (average * 100).roundToInt()
 }
