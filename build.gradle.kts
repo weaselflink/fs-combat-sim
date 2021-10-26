0a406c20ad7ce2d7eded17ddfacccefc3eda31c3
weaselflink <stefan@stefanbissell.de>
2021-10-26 12:21:20 +0200

    get rid of java language version warning; run tests in parallel

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val junit_version: String by project
 val strikt_version: String by project
+val java_version: String by project
 
 plugins {
     application
@@ -24,14 +25,21 @@ dependencies {
     testImplementation("io.strikt:strikt-core:$strikt_version")
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(java_version))
+    }
+}
+
 tasks {
     withType<KotlinCompile> {
         kotlinOptions {
-            jvmTarget = "15"
+            jvmTarget = java_version
         }
     }
 
     test {
+        maxParallelForks = Runtime.getRuntime().availableProcessors()
         useJUnitPlatform()
         testLogging {
             events("passed", "skipped", "failed")
